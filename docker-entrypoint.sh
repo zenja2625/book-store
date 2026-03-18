a42fba0cb91fa29de429c8488ecbe0e794ecd9ff
Евгений <zenja2625@gmail.com>
2026-03-18 02:01:12 +0200

    chore: add permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,18 +6,27 @@ if [ ! -f "/var/www/html/artisan" ]; then
     echo "Artisan not found. Copying October CMS from cache..."
     # Копируем ядро, не перезаписывая ваши локальные папки темы/плагинов (-n)
     cp -rn /usr/src/october/. /var/www/html/
-    
-    mkdir -p storage/framework/cache/data \
-             storage/framework/sessions \
-             storage/framework/views \
-             storage/cms/cache \
-             storage/logs
              
     echo "Files copied successfully!"
 fi
 
-# Всегда проверяем права перед стартом (особенно важно для storage)
-chown -R www-data:www-data storage themes plugins app
+mkdir -p storage/framework/cache/data \
+         storage/framework/sessions \
+         storage/framework/views \
+         storage/cms/cache \
+         storage/cms/combiner \
+         storage/cms/twig \
+         storage/logs \
+         storage/temp
+
+# 3. ФИКС ПРАВ (Самый важный этап)
+echo "Fixing permissions..."
+# Делаем www-data владельцем всего
+chown -R www-data:www-data /var/www/html
+
+# Даем права на запись (775 позволяет и владельцу, и группе писать в папки)
+find /var/www/html/storage -type d -exec chmod 775 {} \;
+find /var/www/html/storage -type f -exec chmod 664 {} \;
 
 if [ "$RUN_MIGRATIONS" = "true" ]; then
     echo "Running migrations..."
